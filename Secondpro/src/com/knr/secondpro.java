a4eebcabb78059d0c2072d30a20803f1ec2b8a3d
narendra <narendra@TO-761XH32-D.corp.techouts.local>
2021-02-26 08:36:01 -0500

    ok

---
+++ Secondpro/src/com/knr/secondpro.java
@@ -0,0 +1,11 @@
+package com.knr;
+
+public class secondpro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("second progreamm");
+
+	}
+
+}
